ae8040630bc6604d384ab0449f0aaef0cc41cb02
vtorres96 <victorcdc96@gmail.com>
2019-09-01 15:35:27 -0300

    adding model Filme

---
+++ app/Filme.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Filme extends Model
+{
+    protected $table = "filmes";
+    protected $primaryKey = "id";
+    protected $fillable = [
+        "titulo", "sinopse", "imagem", "id_protagonista", "id_genero"
+    ];
+
+    public function genero(){
+        return $this->hasOne(Genero::class, 'id', 'id_genero');
+    }
+
+    public function ator(){
+        return $this->hasOne(Ator::class, 'id', 'id_protagonista');
+    }
+}
