b3cad67d24d70c6a42a726b5617c46d9ce651bfd
vtorres96 <victorcdc96@gmail.com>
2019-09-01 16:58:33 -0300

    adding home page of unlogged users

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.master')
+
+@section('title', 'Blockbuster DH - Home')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
+
+                    <div class="card-body">
+                        Seja Bem-Vindo Usuário não logado
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
