637c7449cf269b64d66aba67cd18072264754dd4
vtorres96 <victorcdc96@gmail.com>
2019-09-01 15:39:24 -0300

    adding Ator controller

---
+++ app/Http/Controllers/AtorController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Ator;
+
+class AtorController extends Controller
+{
+    public function listandoAtores(){
+        $atores = Ator::orderBy('id', 'ASC')->paginate(5);
+
+        return view('listandoAtores')->with('atores', $atores);
+    }
+
+    public function adicionandoAtor(){
+        return view('adicionandoAtor');
+    }
+
+    public function salvandoAtor(Request $request){
+        $request->validate([
+            "nome" => "required|max:50"
+        ]);
+
+        $ator = Ator::create([
+            "nome" => $request->input('nome')
+        ]);
+
+        $ator->save();
+
+        return redirect('/atores');
+    }
+
+    public function modificandoAtor($id){
+        $ator = Ator::find($id);
+
+        return view('adicionandoAtor')->with('ator', $ator);
+    }
+
+    public function alterandoAtor(Request $request, $id){
+        $ator = Ator::find($id);
+
+        $request->validate([
+            "nome" => "required|max:50"
+        ]);
+
+        $ator->nome = $request->input('nome');
+
+        $ator->save();
+
+        return redirect('/atores');
+    }
+
+    public function removendoAtor($id){
+        $ator = Ator::find($id);
+
+        $ator->delete();
+
+        return redirect('/atores');
+    }
+}
