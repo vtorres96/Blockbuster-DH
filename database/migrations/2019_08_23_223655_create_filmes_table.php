be451b0706b28dc6273f35603bc5c5469dd9a125
vtorres96 <victorcdc96@gmail.com>
2019-09-01 16:08:07 -0300

    adding migration for movies

---
+++ database/migrations/2019_08_23_223655_create_filmes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilmesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('filmes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('titulo');
+            $table->string('sinopse');
+            $table->string('imagem');
+            $table->unsignedBigInteger('id_protagonista');
+            $table->unsignedBigInteger('id_genero');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('filmes');
+    }
+}
