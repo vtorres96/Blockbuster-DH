bde41d9f9ff4984b5c604bcc3fa295bf1ae5cb40
vtorres96 <victorcdc96@gmail.com>
2019-11-20 15:12:29 -0300

    added FIlmeRequest

--- app/Http/Requests/FilmeRequest.php
+++ app/Http/Requests/FilmeRequest.php
@@ -13,7 +13,7 @@ class FilmeRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,10 @@ class FilmeRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'titulo' => 'required|max:50',
+            'sinopse' => 'required|max:200',
+            'genero' => 'required',
+            'ator' => 'required'
         ];
     }
 }
